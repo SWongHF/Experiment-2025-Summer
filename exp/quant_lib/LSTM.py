f29e5211e9e109cd63d0a12936f7dba8028c92dc
SWongHF <wongsunny4@gmail.com>
2025-08-02 01:47:50 +0800

    Updating LSTM.

--- exp/quant_lib/LSTM.py
+++ exp/quant_lib/LSTM.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8ea17718636de9b50392d0bc94e9e6440e3b17bb91933c8cda492b738d9bac5
-size 1474
+oid sha256:48a47bafe533afca8742f5cf64024c6ce863801cd6f0c888fb4f162993c08a13
+size 4366
